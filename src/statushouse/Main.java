7905e5875f79415f3c0d0930ecec8b02cc191339
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-06 17:29:41 +0900

    feat: add package

--- src/statushouse/Main.java
+++ src/statushouse/Main.java
@@ -1,10 +1,14 @@
 package statushouse;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		Scanner scanner = new Scanner(System.in);
 
 	}
 
+	scanner close();
 }
