152a03a4eaf7baa59e9f3fc50a190cfa1ff354c7
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-07 17:50:30 +0900

    feat: create VisitorLogRepository

--- src/statushouse/repository/VisitorLogRepository.java
+++ src/statushouse/repository/VisitorLogRepository.java
@@ -8,7 +8,32 @@ import statushouse.model.VisitorLog;
 public class VisitorLogRepository {
 	private List<VisitorLog> visitorLogs = new ArrayList<>();
 
+	// save
 	public void save(VisitorLog log) {
 		visitorLogs.add(log);
+	}
+
+	// findAll
+	public List<VisitorLog> findAll() {
+		return visitorLogs;
+	}
+
+	// findById
+	public VisitorLog findById(int id) {
+		for (int i = 0; i < visitorLogs.size(); i++) {
+			VisitorLog log = visitorLogs.get(i);
+			if (log.getId() == id) {
+				return log;
+			}
+		}
+		return null;
+	}
+
+	// delete
+	public void delete(int id) {
+		VisitorLog log = findById(id);
+		if (log != null) {
+			visitorLogs.remove(log);
+		}
 	}
 }
