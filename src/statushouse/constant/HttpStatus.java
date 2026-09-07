8163daff3fb6dea9b58f07bb9442e99dfd7e44a4
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-07 19:57:19 +0900

    feat: create HttpStatusCode

--- src/statushouse/constant/HttpStatus.java
+++ src/statushouse/constant/HttpStatus.java
@@ -1,5 +1,27 @@
 package statushouse.constant;
 
-public class HttpStatus {
+public enum HttpStatus {
+	OK(200, "OK"), CREATED(201, "Created"), NO_CONTENT(204, "No Content"), BAD_REQUEST(400,
+			"Bad Request"), NOT_FOUND(404, "Not Found"), INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+	private final int code;
+	private final String message;
+
+	/**
+	 * @param code
+	 * @param message
+	 */
+	private HttpStatus(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
 
 }
