38a892ea5f5ff9dbe0033b2fa3bd67646262fcf3
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-07 20:32:18 +0900

    feat: create VisitorLogCreateService

--- src/statushouse/service/VisitorLogCreateService.java
+++ src/statushouse/service/VisitorLogCreateService.java
@@ -1,5 +1,16 @@
 package statushouse.service;
 
+import statushouse.repository.VisitorLogRepository;
+
 public class VisitorLogCreateService {
+
+	private VisitorLogRepository repository;
+
+	/**
+	 * @param repository
+	 */
+	public VisitorLogCreateService(VisitorLogRepository repository) {
+		this.repository = repository;
+	}
 
 }
