a01ddb3b0ba183a09f26caa8b1e173cb7b0937bb
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-08 10:37:51 +0900

    feat: create VisitorLogLeadServise

--- src/statushouse/service/VisitorLogReadService.java
+++ src/statushouse/service/VisitorLogReadService.java
@@ -1,5 +1,22 @@
 package statushouse.service;
 
-public class VisitorLogReadService {
+import java.util.List;
 
+import statushouse.model.VisitorLog;
+import statushouse.repository.VisitorLogRepository;
+
+public class VisitorLogReadService {
+	private VisitorLogRepository repository;
+
+	public VisitorLogReadService(VisitorLogRepository repository) {
+		this.repository = repository;
+	}
+
+	public List<VisitorLog> findAll() {
+		return repository.findAll();
+	}
+
+	public VisitorLog findById(int id) {
+		return repository.findById(id);
+	}
 }
