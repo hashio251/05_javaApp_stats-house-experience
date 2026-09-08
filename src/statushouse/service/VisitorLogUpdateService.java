c1739dc4adefe741f142c86fcce2dd46ef755e2d
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-08 11:18:58 +0900

    feat: create VisitorLogUpdateServce

--- src/statushouse/service/VisitorLogUpdateService.java
+++ src/statushouse/service/VisitorLogUpdateService.java
@@ -1,5 +1,27 @@
 package statushouse.service;
 
+import statushouse.model.VisitorLog;
+import statushouse.repository.VisitorLogRepository;
+
 public class VisitorLogUpdateService {
+	private VisitorLogRepository repository;
+
+	/**
+	 * @param repository
+	 */
+	public VisitorLogUpdateService(VisitorLogRepository repository) {
+		this.repository = repository;
+	}
+
+	public VisitorLog update(int id, String visitorName, int roomCode, String message) {
+		VisitorLog log = repository.findById(id);
+		if (log != null) {
+			log.setVisitorName(visitorName);
+			log.setRoomCode(roomCode);
+			log.setMessage(message);
+			return log;
+		}
+		return null;
+	}
 
 }
