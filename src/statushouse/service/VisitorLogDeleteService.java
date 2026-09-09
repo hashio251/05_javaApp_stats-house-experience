9121d6ff9d1be3d04caa161002c68ab433a64a3f
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-09 16:00:45 +0900

    feat: create VisitorLogDeleteService class

--- src/statushouse/service/VisitorLogDeleteService.java
+++ src/statushouse/service/VisitorLogDeleteService.java
@@ -1,5 +1,25 @@
 package statushouse.service;
 
-public class VisitorLogDeleteService {
+import statushouse.model.VisitorLog;
+import statushouse.repository.VisitorLogRepository;
 
+public class VisitorLogDeleteService {
+	private VisitorLogRepository repository;
+
+	/**
+	 * @param repository
+	 */
+	public VisitorLogDeleteService(VisitorLogRepository repository) {
+		this.repository = repository;
+	}
+
+	public VisitorLog delete(int id) {
+		VisitorLog log = repository.findById(id);
+
+		if (log != null) {
+			repository.delete(id);
+			return log;
+		}
+		return null;
+	}
 }
