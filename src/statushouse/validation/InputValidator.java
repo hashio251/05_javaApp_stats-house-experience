ffbe6ce5bc4b9ce60ea8c271bd025ec4815cf72f
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-09 17:21:25 +0900

    feat: input InputValidator class

--- src/statushouse/validation/InputValidator.java
+++ src/statushouse/validation/InputValidator.java
@@ -1,5 +1,10 @@
 package statushouse.validation;
 
 public class InputValidator {
-
+	public boolean isEmpty(String input) {
+		if (input == null || input.trim().isEmpty()) {
+			return true;
+		}
+		return false;
+	}
 }
