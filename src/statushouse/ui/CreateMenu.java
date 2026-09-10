d4cd991dfaff9e23fe6bab8b1b71aae39e465e50
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-10 11:05:30 +0900

    del: import java.util.jar.Attributes.Name;

--- src/statushouse/ui/CreateMenu.java
+++ src/statushouse/ui/CreateMenu.java
@@ -1,5 +1,27 @@
 package statushouse.ui;
 
+import java.util.Scanner;
+
+import statushouse.service.VisitorLogCreateService;
+
 public class CreateMenu {
+	private Scanner scanner;
+	private VisitorLogCreateService createService;
+
+	/**
+	 * @param scanner
+	 * @param createService
+	 */
+	public CreateMenu(Scanner scanner, VisitorLogCreateService createService) {
+		this.scanner = scanner;
+		this.createService = createService;
+	}
+
+	public void show() {
+		System.out.print("Visitor Name: ");
+		String visitorName = scanner.nextLine();
+
+		System.out.println("Visitor Name: " + visitorName);
+	}
 
 }
