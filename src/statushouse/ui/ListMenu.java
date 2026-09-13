635c42a75a1a459042cb2503c0910a42f3d84d83
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-13 15:35:03 +0900

    feat; function about return allviewlist

--- src/statushouse/ui/ListMenu.java
+++ src/statushouse/ui/ListMenu.java
@@ -1,5 +1,19 @@
 package statushouse.ui;
 
+import statushouse.model.VisitorLog;
+import statushouse.service.VisitorLogReadService;
+
 public class ListMenu {
+	private VisitorLogReadService readService;
+
+	public ListMenu(VisitorLogReadService readService) {
+		this.readService = readService;
+	}
+
+	public void show() {
+		List<VisitorLog> logs = readService.findAll();
+		
+		if (logs.())
+}
 
 }
