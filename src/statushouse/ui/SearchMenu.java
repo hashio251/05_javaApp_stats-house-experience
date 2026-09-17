b12bfb9568495367372f02be7e1fa7ab40c72d74
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-17 17:32:16 +0900

    feat: create SearchMenu

--- src/statushouse/ui/SearchMenu.java
+++ src/statushouse/ui/SearchMenu.java
@@ -1,5 +1,52 @@
 package statushouse.ui;
 
+import java.util.Scanner;
+
+import statushouse.model.VisitorLog;
+import statushouse.service.VisitorLogReadService;
+import statushouse.validation.InputValidator;
+
 public class SearchMenu {
+	private Scanner scanner;
+	private VisitorLogReadService readService;
+	private InputValidator validator;
+
+	/**
+	 * @param scanner
+	 * @param readService
+	 * @param validator
+	 */
+	public SearchMenu(Scanner scanner, VisitorLogReadService readService, InputValidator validator) {
+		this.scanner = scanner;
+		this.readService = readService;
+		this.validator = validator;
+	}
+
+	public void show() {
+		System.out.print("Search ID: ");
+		String idInput = scanner.nextLine();
+
+		while (!validator.isNumber(idInput)) {
+			System.out.println("数字を入力してください。");
+			idInput = scanner.nextLine();
+		}
+
+		int visitorIntegerId = Integer.parseInt(idInput);
+
+		VisitorLog log = readService.findById(visitorIntegerId);
+
+		if (log == null) {
+			System.out.println("404 Not Found");
+			return;
+		}
+
+		System.out.println("200 OK");
+		System.out.println("ID: " + log.getId());
+		System.out.println("Visitor Name: " + log.getVisitorName());
+		System.out.println("Room Code: " + log.getRoomCode());
+		System.out.println("Message: " + log.getMessage());
+		System.out.println("Visited At: " + log.getVisitedAt());
+
+	}
 
 }
