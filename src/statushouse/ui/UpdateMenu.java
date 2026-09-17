d8d974fc128df61fc2c1c4caeb3477050a8667b4
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-17 19:53:10 +0900

    feat: create UpdateService

--- src/statushouse/ui/UpdateMenu.java
+++ src/statushouse/ui/UpdateMenu.java
@@ -1,5 +1,57 @@
 package statushouse.ui;
 
-public class UpdateMenu {
+import java.util.Scanner;
 
+import statushouse.model.VisitorLog;
+import statushouse.service.VisitorLogReadService;
+import statushouse.service.VisitorLogUpdateService;
+import statushouse.validation.InputValidator;
+
+public class UpdateMenu {
+	private Scanner scanner;
+	private VisitorLogReadService readService;
+	private VisitorLogUpdateService updateService;
+	private InputValidator validator;
+
+	/**
+	 * @param scanner
+	 * @param readService
+	 * @param updateService
+	 * @param inputValidator
+	 */
+	public UpdateMenu(Scanner scanner, VisitorLogReadService readService, VisitorLogUpdateService updateService,
+			InputValidator validator) {
+		this.scanner = scanner;
+		this.readService = readService;
+		this.updateService = updateService;
+		this.validator = validator;
+	}
+
+	public void show() {
+		System.out.println("Edit ID: ");
+		String idInput = scanner.nextLine();
+
+		while (!validator.isNumber(idInput)) {
+			System.out.println("数字を入力してください。");
+			System.out.print("Edit ID: ");
+			idInput = scanner.nextLine();
+		}
+
+		int id = Integer.parseInt(idInput);
+
+		VisitorLog log = readService.findById(id);
+
+		if (log == null) {
+			System.out.println("404 Not Found");
+			return;
+		}
+
+		System.out.println("200 OK");
+		System.out.println("ID: " + log.getId());
+		System.out.println("Visitor Name: " + log.getVisitorName());
+		System.out.println("Room Code: " + log.getRoomCode());
+		System.out.println("Message: " + log.getMessage());
+		System.out.println("Visited At: " + log.getVisitedAt());
+
+	}
 }
