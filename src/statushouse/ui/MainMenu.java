1d20ab41b75a1f7d6ef11a0d603d0a74a355d792
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-09-24 19:05:13 +0900

    fix: cli ui

--- src/statushouse/ui/MainMenu.java
+++ src/statushouse/ui/MainMenu.java
@@ -23,6 +23,7 @@ public class MainMenu {
 		System.out.println("6. Search by ID");
 		System.out.println("0. Exit");
 		System.out.println("========================");
+		System.out.println();
 
 		String input = scanner.nextLine();
 		if (!validator.isValidMenu(input)) {
